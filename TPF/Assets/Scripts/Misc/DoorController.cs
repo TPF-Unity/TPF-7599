78ce89518e9709eccfef245df3bdbfcb7727a51c
Melina Lazzaro <melinalazzaro01@gmail.com>
2024-01-20 16:02:49 -0300

    win and game over screens

--- TPF/Assets/Scripts/Misc/DoorController.cs
+++ TPF/Assets/Scripts/Misc/DoorController.cs
@@ -1,23 +1,26 @@
 using System.Collections;
 using System.Collections.Generic;
+using StarterAssets;
 using UnityEngine;
 
 public class DoorController : MonoBehaviour
 {
-
     [SerializeField]
     private bool open = false;
 
     private Renderer doorRenderer;
+    private SceneLoader sceneLoader;
 
     private void Start () {
         doorRenderer = GetComponent<Renderer> ();
         doorRenderer.material.color = Color.red;
+        sceneLoader = GameObject.Find("SceneLoader").GetComponent<SceneLoader>();
+
     }
 
     private void OnTriggerEnter (Collider other) {
-        if (other.CompareTag ("Player") && open) {
-            Debug.Log ("You won!");
+        if (other.CompareTag("Player") && open) {
+            sceneLoader.LoadGameWinScene();
         }
     }
 
