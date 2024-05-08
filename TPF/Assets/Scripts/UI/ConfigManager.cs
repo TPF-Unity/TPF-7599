61a494093cf886b8a6a6a56cc459847371467531
Nahuel Castro <68122322+nauee@users.noreply.github.com>
2024-05-08 11:23:47 -0300

    Difficulty management

---
+++ TPF/Assets/Scripts/UI/ConfigManager.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+
+[DefaultExecutionOrder(10000)] // Ensures UI initializes last
+public class ConfigManager : MonoBehaviour
+{
+    public void SetDifficulty(string difficulty)
+    {
+        PlayerPrefs.SetString("Difficulty", difficulty);
+        Debug.Log("Difficulty set to " + PlayerPrefs.GetString("Difficulty", "MISSING"));
+    }
+
+}
